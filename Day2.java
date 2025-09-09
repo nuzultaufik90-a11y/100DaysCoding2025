68aa92258d3ea2ca3a972a8832ee9525eb711f18
NuzulT <nuzultaufik90@gmail.com>
2025-09-09 10:58:55 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,14 @@
+public class Day2 {
+
+    
+    public static void main(String[] args) {
+            System.out.printf("Halo %s, umur kamu %d tahun\n", "nuzul", 20);
+            
+            System.out.println("saya nuzul taufik");
+            
+            System.out.print("nuzul taufik");
+            
+            
+    }
+    
+}

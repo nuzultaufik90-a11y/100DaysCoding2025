f03efb4f890d60b0bb557c929f3edfc194d4fb4c
NuzulT <nuzultaufik90@gmail.com>
2025-09-10 20:11:19 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,23 @@
+package day3;
+
+
+public class Day3 {
+
+    
+    public static void main(String[] args) {
+            System.out.printf("Halo %s, umur kamu %d tahun\n", "nuzul", 20);
+            // printf untuk mencetak karakter terformat,
+            //mirip dengan print,tetapi dengan format tertentu.
+            
+            System.out.println("saya nuzul taufik");
+            //println untuk mencetak dengan membuat baris baru setelahnya.
+            
+            System.out.print("nuzul taufik");
+            //print untuk mencetak tanpa membuat baris baru.
+            
+            // Karena materi day 3 itu adalah 'komentar'
+            // maka saya kasih gabung saja materi day 2.
+            
+    }
+    
+}

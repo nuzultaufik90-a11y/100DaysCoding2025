edd1de09bf74e13a70666efffcaf56a9d8f64dd2
NuzulT <nuzultaufik90@gmail.com>
2025-09-11 10:55:30 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,30 @@
+package day4;
+
+import java.util.Calendar;
+
+
+public class Day4 {
+
+    
+    public static void main(String[] args) {
+            System.out.printf("Halo %s, umur kamu %d tahun\n", "nuzul", 20);
+            // printf untuk mencetak karakter terformat,
+            //mirip dengan print,tetapi dengan format tertentu.
+            
+            System.out.println("saya nuzul taufik");
+            //println untuk mencetak dengan membuat baris baru setelahnya.
+            
+            System.out.print("nuzul taufik\n");
+            //print untuk mencetak tanpa membuat baris baru.
+            
+            // Karena materi day 3 itu adalah 'komentar'
+            // maka saya kasih gabung saja di materi day 2.
+            
+            //Karena materi day 4 juga sama dengan materi day 2
+            //maka saya kasih gabung juga di materi day 2
+            // yakni pembahasan mengenai Penggunaan Escape Sequence
+            // contohnya seperti (\n, \t, \b)
+            
+    }
+    
+}

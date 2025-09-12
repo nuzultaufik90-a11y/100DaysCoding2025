d630519f92e2f7806b6d44cddb5d4e23cf899f28
NuzulT <nuzultaufik90@gmail.com>
2025-09-12 11:31:13 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,23 @@
+package day5;
+
+public class Day5 {
+
+
+    public static void main(String[] args) {
+        byte angka1 = 100; 
+        //Rentang Nilai: Dari -128 hingga 127.
+        short angka2 = 32767; 
+        //Rentang nilai: dari -32,768 hingga 32,767.
+        int angka3 = 2147483647;
+        //rentang nilai dari -2,147,483,648 hingga 2,147,483,647
+        long angka4 = 1000000001;
+        //Rentang nilai: dari -9,223,372,036,854,775,808 hingga 
+        //9,223,372,036,854,775,807
+        
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.println(angka3);
+        System.out.println(angka4);
+    }
+    
+}

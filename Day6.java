949ca2a3ddd560cbb1d8eff9471f3d79b2ea9aff
NuzulT <nuzultaufik90@gmail.com>
2025-09-13 19:33:04 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,13 @@
+public class Day6 {
+    
+    public static void main(String[] args) {
+        double panjang = 178.4;
+      //Rentang Nilai: -1.7976931348623157E+308 hingga 
+      //+1.7976931348623157E+308 (sekitar 1,8 x 10^308).
+        float jarak = 3.5f;
+      //Rentang Nilai: 1.4E-45 hingga 3.4028235E+38. 
+        System.out.println(panjang);
+        System.out.println(jarak);
+    }
+    
+}

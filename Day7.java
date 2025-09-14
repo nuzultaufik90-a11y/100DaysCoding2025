d5717442258797c1019730deea994905580769b4
NuzulT <nuzultaufik90@gmail.com>
2025-09-14 09:22:15 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class Day5 {
+
+
+    public static void main(String[] args) {
+       boolean sehat = true;
+       boolean sakit = false;
+       char gender = 'L';
+       
+        System.out.println(sehat);
+        System.out.println(sakit);
+        System.out.println(gender);
+      
+    }
+    
+}

f1277b3e99ef677143393e524897d13ac2f490e8
NuzulT <nuzultaufik90@gmail.com>
2025-09-15 22:05:23 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+public class Day8 {
+    
+    public static void main(String[] args) {
+        //String digunakan untuk menyimpan teks 
+        String nama = "nuzul";
+        String nim = "D0225345";
+        
+        System.out.println(nama);
+        System.out.println(nim);
+    }
+    
+}

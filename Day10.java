f331294f604e684d91fa11323d430cd05f5ab668
NuzulT <nuzultaufik90@gmail.com>
2025-09-17 18:37:03 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,16 @@
+package file.ujicoba;
+
+public class FileUjicoba {
+
+    public static void main(String[] args) { 
+    final double tinggi = 165.4;
+    
+    //Variabel final adalah konstan yang nilainya tidak dapat diubah setelah inisialisasi. 
+    //Variabel final biasanya digunakan untuk menyimpan nilai yang bersifat tetap, 
+    //seperti konstanta matematis.
+    
+        System.out.println("tinggi saya = " + tinggi);
+    
+    }
+    
+}

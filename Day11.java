1d751e2b0ca8754ed8b2c6e4d02538db1d4c02f5
NuzulT <nuzultaufik90@gmail.com>
2025-09-18 12:07:09 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,17 @@
+package Day11;
+import java.util.Scanner;
+
+public class Day11 {
+
+    
+    public static void main(String[] args) {
+    int angka;
+    
+    Scanner input = new Scanner(System.in);
+    
+    angka = input.nextInt();
+    
+        System.out.println("angka = " + angka);
+    }
+    
+}

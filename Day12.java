da93791c5e51599322636c7e9ed579a819482427
NuzulT <nuzultaufik90@gmail.com>
2025-09-19 12:00:04 +0800

    Day12.java

---
+++ Day12.java
@@ -0,0 +1,24 @@
+public class Day12 {
+
+    public static void main(String[] args) {
+        String nama;
+        int usia;
+        double tinggi;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("masukan nama : ");
+        nama = input.nextLine();
+        System.out.print("masukan usia : ");
+        usia = input.nextInt();
+        System.out.print("masukan tinggi : ");
+        tinggi = input.nextDouble();
+        
+        System.out.println("\nbiodata mahasiswa :");
+        System.out.printf("Nama Lengkap : " + nama);
+        System.out.println("\nUsia : " + usia);
+        System.out.print("Tinggi Badan : " + tinggi);
+        
+    }
+    
+}

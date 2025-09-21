525ea09c482e207e9aa26beabca89ec24fcf1c24
NuzulT <nuzultaufik90@gmail.com>
2025-09-21 21:02:24 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,11 @@
+public class Day14 {
+    
+    public static void main(String[] args) {
+        String nama = "164";
+        int saya = integer.parseInt(nama);
+        
+        System.out.println(saya);
+        
+    }
+    
+}

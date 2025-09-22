05c2db0d44ee20c08f7e69aa89403be7da234970
NuzulT <nuzultaufik90@gmail.com>
2025-09-22 22:13:16 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,18 @@
+package Day15;
+public class Day15 {
+
+    public static void main(String[] args) {
+        
+        int angka1 = 10; 
+        int angka2 = 5;
+        int angka3 = 10;
+        int angka4 = 5;
+        int ditambah = angka1+angka2;
+        int dikurang = angka3-angka4;
+        System.out.println("hasil penjumlahan: "+ditambah );
+        System.out.println("hasil penjumlahan: "+dikurang ); 
+        
+        
+    }
+    
+}

8b6bc2e66e28f4bbca7e156aca862c842eab462f
NuzulT <nuzultaufik90@gmail.com>
2025-09-25 18:02:19 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,12 @@
+package Day18;
+
+public class Day18 {
+    public static void main(String [] args){
+        byte a = 20;       
+        int b = a;    
+        
+        System.out.println("byte: " + a);
+        System.out.println("int: " + b);
+    }
+    
+}

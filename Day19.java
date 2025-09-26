1df5ee18365a74d2d042789ed291c2e33411f10e
NuzulT <nuzultaufik90@gmail.com>
2025-09-26 21:57:07 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,11 @@
+package Day19;
+
+public class Day19 {
+
+    public static void main(String[] args) {
+          short angka1 = 180 ;
+		      byte angkabyte = (byte) angka1;
+		      System.out.println(angkabyte);
+    }
+    
+}

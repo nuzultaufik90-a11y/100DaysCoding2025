92b729f016d8e3630162a3958469a27661a7ea3e
NuzulT <nuzultaufik90@gmail.com>
2025-09-27 21:11:48 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,11 @@
+package Day20;
+
+public class Day20 {
+
+    public static void main(String[] args) {
+        int angka = 777;
+        String teks = String.valueOf(angka);
+        System.out.println(teks); 
+    }
+    
+}

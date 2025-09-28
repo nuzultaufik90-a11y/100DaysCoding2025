4bdf0596ca3f607fb24146dd436b0b376929ffd1
NuzulT <nuzultaufik90@gmail.com>
2025-09-28 18:57:56 +0800

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,15 @@
+package Day21;
+public class Day21 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+          
+        a = a + b;  // = 30
+        b = a - b;  // = 10
+        a = a - b;  // = 20
+
+        System.out.println("a = " + a + ", b = " + b);
+    }
+    
+}

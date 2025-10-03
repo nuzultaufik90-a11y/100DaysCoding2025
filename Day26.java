0d27c612e36cddd62d9e5b7e7d617bd60c491c00
NuzulT <nuzultaufik90@gmail.com>
2025-10-03 18:19:04 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,25 @@
+package Day26;
+
+public class Day26 {
+
+    public static void main(String[] args) {
+        int nilai = 5;
+        System.out.println("nilai awal: "+ nilai);
+        
+        nilai += 5;
+        System.out.println("nilai +=5: "+ nilai);
+        
+        nilai -= 4;
+        System.out.println("nilai -=4: "+ nilai);
+        
+        nilai *= 3;
+        System.out.println("nilai *=3: "+ nilai);
+        
+        nilai /= 2;
+        System.out.println("nilai /=2: "+ nilai);
+        
+        nilai %= 1;
+        System.out.println("nilai %=1: "+ nilai);
+    }
+    
+}

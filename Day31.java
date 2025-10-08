369f54ef7aa388610b8830565ce8d796c4a1a3c1
NuzulT <nuzultaufik90@gmail.com>
2025-10-08 13:26:14 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,15 @@
+package perbandinganpart1;
+
+public class Day31 {
+    public static void main(String[] args) {
+       boolean a = true;
+       boolean b = false;
+       
+       boolean hasil = a && b;
+       
+        System.out.println("nilai a: "+ a);
+        System.out.println("nilai b: "+ b);
+        System.out.println("nilai a dan b: "+ hasil);
+        
+    }
+}

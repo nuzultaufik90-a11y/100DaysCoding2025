e0e2bcfb4b7803b20197665f02dee1bc9436c0e9
NuzulT <nuzultaufik90@gmail.com>
2025-10-09 19:37:28 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,15 @@
+package Day32;
+
+public class Day32 {
+    public static void main(String[] args) {
+        int angka1 = 80;
+        int angka2 = 30;
+        
+        boolean hasil = ( angka1 > 70) || ( angka2 > 40 );
+        
+        System.out.println("angka1: "+ angka1);
+        System.out.println("angka2: "+ angka2);
+        System.out.println("apakah salah satu angka lebih besar dari 70: "+ hasil);
+    }
+    
+}

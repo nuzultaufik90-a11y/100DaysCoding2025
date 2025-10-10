8664aab4b5c35aa6b4c2a1be80effc73294fd43d
NuzulT <nuzultaufik90@gmail.com>
2025-10-10 17:14:06 +0800

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,12 @@
+package Day33;
+
+public class Day33 {
+    public static void main(String[] args) {
+       boolean benar = true;
+       boolean salah = false;
+    
+        System.out.println("jawaban benar "+ !benar);
+        System.out.println("jawaban salah "+ !salah);
+    }
+    
+}

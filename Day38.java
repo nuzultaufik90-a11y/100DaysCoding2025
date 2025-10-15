656fe98f671991327922b32a2f15d0facfd20a93
NuzulT <nuzultaufik90@gmail.com>
2025-10-15 17:08:13 +0800

    Day38.java

---
+++ Day38.java
@@ -0,0 +1,25 @@
+package Day38;
+import java.util.Scanner;
+public class Day38 {
+
+    
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+        
+        System.out.print("masukkan bilangan: ");
+        int angka = input.nextInt();
+        
+        if(angka > 0){
+            System.out.println("ini angka positif ");
+            
+        }
+        else if(angka < 0){
+            System.out.println("ini angka negatif ");
+        }
+        else{
+            System.out.println("ini angka nol ");
+        }
+    }
+    
+    
+}

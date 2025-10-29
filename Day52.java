563fc579db0738bfffa6c7311ded8f83e78109e7
NuzulT <nuzultaufik90@gmail.com>
2025-10-29 18:20:55 +0800

    Day52.java

---
+++ Day52.java
@@ -0,0 +1,13 @@
+package Day52;
+
+public class Day52 {
+    public static void main(String[] args) {
+         int i = 1;
+
+        while (i <= 5) {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }
+        
+    }
+}

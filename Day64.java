fa38adaac46ac1018338e03e49cb387d3816a6a3
NuzulT <nuzultaufik90@gmail.com>
2025-11-10 19:11:02 +0800

    Day64.java

---
+++ Day64.java
@@ -0,0 +1,21 @@
+package Day64;
+import java.util.Scanner;
+
+public class Day64 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai M: ");
+        int M = input.nextInt();
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+
+        int hasil = 1;
+        
+        for (int i = 1; i <= N; i++) {
+            hasil = hasil * M;
+        }
+       
+        System.out.println("Hasil dari " + M + " pangkat " + N + " adalah: " + hasil);
+    }
+}

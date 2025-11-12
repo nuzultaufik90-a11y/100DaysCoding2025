5aed36be5118a255eacb000165a5a66ff1f70f56
NuzulT <nuzultaufik90@gmail.com>
2025-11-12 18:20:44 +0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,14 @@
+package Day66;
+
+public class Day66 {
+
+    public static void main(String[] args) {
+        
+        int T = 7;
+        for (int i = 0; i < T; i++) {
+            System.out.println("*");
+            
+        }
+    }
+    
+}

f30db13269462656fe7664b4b2bc721ef64b6632
NuzulT <nuzultaufik90@gmail.com>
2025-11-13 19:19:29 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,13 @@
+package Day67;
+
+public class Day67 {
+    public static void main(String[] args) {
+        int T = 10;
+        for (int i = 0; i < T; i++) {
+            System.out.print(" * ");
+            
+        }
+        System.out.println();
+    }
+    
+}
